ea62aef8048843043f347dc5883218558e67b848
Justus Adam <dev@justus.science>
2016-07-05 15:28:03 +0200

    Reworked run script

--- scripts/RunTests.hs
+++ scripts/RunTests.hs
@@ -9,16 +9,29 @@ import           Control.Monad
 import           Data.Foldable (for_)
 import           Data.Monoid
 import qualified Data.Text     as T
-import           Prelude       hiding (FilePath)
-import           Shelly
+import           Prelude       hiding (FilePath, (++))
+import           Shelly as Shelly
 import           Text.Printf
+import qualified Text.Regex.PCRE.Heavy as Re
+import qualified Text.Regex.PCRE.Light as Re
+import Data.String (fromString)
+import Data.Maybe
 
 
 default (T.Text)
 
 
+(++) :: Monoid a => a -> a -> a
+(++) = mappend
+
+
+graphFilesRegex :: Re.Regex
+graphFilesRegex = Re.compile "(run_test_level(\\d+)_(\\d+)).hs$" []
+
+
+
 graphGenerationBinaryLocation :: FilePath
-graphGenerationBinaryLocation = "../rand-code-graph/dist/build/random-level-graphs/random-level-graphs"
+graphGenerationBinaryLocation = "../rand-code-graph/.stack-work/install/x86_64-osx/lts-6.4/7.10.3/bin/random-level-graphs"
 
 
 outputLocation :: FilePath
@@ -26,15 +39,53 @@ outputLocation = "results"
 
 
 graphsToGenerate :: Int
-graphsToGenerate = 20
+graphsToGenerate = 4
 
 
 maxLevel :: Int
-maxLevel = 20
+maxLevel = 9
 
 
 seed :: T.Text
 seed = "12345"
+
+
+runOne :: T.Text -> Sh ()
+runOne type_ = do
+    echo $ T.pack $ printf "Building and testing %i graphs" (graphsToGenerate * maxLevel)
+    let genPath = "generated"
+    ls genPath >>= mapM_ (\x -> echo ("removing " <> T.pack (show x)) >> rm x) . filter (not . (`elem` [".", ".."]))
+    (generationTime, _) <- time $ run
+        graphGenerationBinaryLocation
+        [ "-L", type_
+        , "-o", "generated/"
+        , "-l", T.pack $ show maxLevel
+        , "-n", T.pack $ show (graphsToGenerate * maxLevel)
+        , "-s", seed
+        ]
+    files <- fmap catMaybes $ (filter (not . (`elem` [".", ".."])) <$> lsT genPath) >>= mapM (\filepath ->
+        case Re.scan graphFilesRegex filepath of
+            [(_, [funname, level, index])] -> do
+                content <- readfile $ fromString $ T.unpack filepath
+                return $ Just $ (content, (funname, level, index :: T.Text))
+            _ -> return Nothing
+        )
+    let (code, functions) = unzip files
+    pream <- readfile "resources/Preamble.hs"
+    let allTests :: T.Text
+        allTests =
+               "allTests :: [(Env u -> IO Int, Int, Int)]\n"
+            <> "allTests = [(" ++ T.intercalate "), (" (map (\(name, level, index) -> T.intercalate "," [name, level, index]) functions) ++ ")]"
+    let fullContent = T.intercalate "\n" $ pream:allTests:code :: T.Text
+    writefile (genPath </> "TestGraphs.hs") fullContent
+
+    echo $ T.pack $ printf "Generated %i graphs in %s" (graphsToGenerate * maxLevel) (formatSeconds $ ceiling generationTime)
+    (compilationTime, _) <- time $ run "cabal" ["build", "haxl-test-generated-graph-exe"]
+    echo $ T.pack $ printf "Compiled test program in %s" (formatSeconds $ ceiling compilationTime)
+    (runTime, _) <- time $ run "./dist/build/haxl-test-generated-graph-exe/haxl-test-generated-graph-exe" [] >>=
+        writefile (outputLocation </> fromString ("haskell-" ++ T.unpack type_) <.>"json")
+    echo $ T.pack $ printf "Ran program in %s" (formatSeconds $ ceiling runTime)
+
 
 
 formatSeconds :: Int -> String
@@ -57,36 +108,5 @@ main = shelly $ print_stdout False $ escaping False $ do
 
     run "cabal" ["install", "--only-dependencies"]
 
-    echo $ T.pack $ printf "Building and testing %i graphs" (graphsToGenerate * maxLevel)
-
-    (generationTime, _) <- time $ run
-        graphGenerationBinaryLocation
-        [ "-L", "HaskellDo"
-        , "-p", "resources/Preamble.hs"
-        , "-o", "generated/TestGraphs.hs"
-        , "-l", T.pack $ show maxLevel
-        , "-n", T.pack $ show (graphsToGenerate * maxLevel)
-        , "-s", seed
-        ]
-    echo $ T.pack $ printf "Generated %i graphs in %s" (graphsToGenerate * maxLevel) (formatSeconds $ ceiling generationTime)
-    (compilationTime, _) <- time $ run "cabal" ["build"]
-    echo $ T.pack $ printf "Compiled test program in %s" (formatSeconds $ ceiling compilationTime)
-    (runTime, _) <- time $ run "dist/build/haxl-test-generated-graph-exe/haxl-test-generated-graph-exe" [] >>=
-        writefile (outputLocation </> "haskell.json")
-    echo $ T.pack $ printf "Ran program in %s" (formatSeconds $ ceiling runTime)
-
-    (generationTime, _) <- time $ run
-        graphGenerationBinaryLocation
-        [ "-L", "HaskellDoApp"
-        , "-p", "resources/Preamble.hs"
-        , "-o", "generated/TestGraphs.hs"
-        , "-l", T.pack $ show maxLevel
-        , "-n", T.pack $ show (graphsToGenerate * maxLevel)
-        , "-s", seed
-        ]
-    echo $ T.pack $ printf "Generated %i graphs in %s" (graphsToGenerate * maxLevel) (formatSeconds $ ceiling generationTime)
-    (compilationTime, _) <- time $ run "cabal" ["build"]
-    echo $ T.pack $ printf "Compiled test program in %s" (formatSeconds $ ceiling compilationTime)
-    (runTime, _) <- time $ run "dist/build/haxl-test-generated-graph-exe/haxl-test-generated-graph-exe" [] >>=
-        writefile (outputLocation </> "haskell-applicative.json")
-    echo $ T.pack $ printf "Ran program in %s" (formatSeconds $ ceiling runTime)
+    runOne "HaskellDo"
+    runOne "HaskellDoApp"
